280e54b86a6b7b1d651e8db774859b77cfb22635
mehak-fabtechsol <mehak@fabtechsol.com>
2024-05-23 18:17:35 +0500

    ireti backend

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -2,51 +2,58 @@
     <div>
         <div class="logo">
             <a href="dashboard.html">
-                <img src="{{asset('img/logo.svg')}}" alt="logo" class="w-100">
+                <img src="{{ asset('img/logo.svg') }}" alt="logo" class="w-100">
             </a>
         </div>
         <div class="lists">
             <ul>
                 <li>
-                    <a href="{{route('admin.dashboard')}}" class="{{ request()->routeIs('admin.dashboard') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
+                    <a href="{{ route('admin.dashboard') }}"
+                        class="{{ request()->routeIs('admin.dashboard') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2 ">
-                            <img src="{{ request()->routeIs('admin.dashboard') ? asset('img/gridg.svg') : asset('img/grid.svg') }}" alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.dashboard') ? asset('img/gridg.svg') : asset('img/grid.svg') }}"
+                                alt="" class="sicon">
                             <p class="mb-0 ">Dashboard</p>
                         </div>
                     </a>
                 </li>
                 <li>
-                    <a href="{{route('admin.orders')}}" class="{{ request()->routeIs('admin.orders') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
+                    <a href="{{ route('admin.orders') }}"
+                        class="{{ request()->routeIs('admin.orders') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.orders') ? asset('img/boxg.svg') : asset('img/box.svg') }}" alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.orders') ? asset('img/ordersg.svg') : asset('img/orders.svg') }}"
+                                alt="" class="sicon">
                             My Orders
                         </div>
                     </a>
                 </li>
                 <li>
-                    <a href="{{route('admin.clients')}}" class="{{ request()->routeIs('admin.clients') ? 'active' : '' }} text-decoration-none d-block lnk text-black">
+                    <a href="{{ route('admin.clients') }}"
+                        class="{{ request()->routeIs('admin.clients') ? 'active' : '' }} text-decoration-none d-block lnk text-black">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.clients') ? asset('img/growg.svg') : asset('img/grow.svg') }}" alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.clients') ? asset('img/clientsg.svg') : asset('img/clients.svg') }}"
+                                alt="" class="sicon">
                             My Clients
                         </div>
                     </a>
                 </li>
                 <li>
-                    <a href="{{route('admin.payments')}}" class="{{ request()->routeIs('admin.payments') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
+                    <a href="{{ route('admin.payments') }}"
+                        class="{{ request()->routeIs('admin.payments') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.payments') ? asset('img/userg.svg') : asset('img/user.svg') }}" alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.payments') ? asset('img/paymentg.svg') : asset('img/payment.svg') }}"
+                                alt="" class="sicon">
                             Payments
                         </div>
                     </a>
                 </li>
-            
             </ul>
         </div>
     </div>
     <div class="lgout">
-        <a href="{{route('login')}}" class="text-decoration-none d-block lnk text-black">
+        <a href="{{ route('login') }}" class="text-decoration-none d-block lnk text-black">
             <div class="log">
-                <img src="{{asset('img/logout.svg')}}" alt="logout" class="sicon">
+                <img src="{{ asset('img/logout.svg') }}" alt="logout" class="sicon">
                 Logout
             </div>
         </a>
