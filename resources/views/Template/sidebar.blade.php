5828763d2365d46f042b0d9714d40639991a8590
shakaib-fabtechsol <shakaib@fabyechsol.com>
2024-05-24 11:38:59 +0500

    all done

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="leftmain bg-white">
     <div>
         <div class="logo">
-            <a href="dashboard.html">
+            <a href="{{route('admin.dashboard')}}">
                 <img src="{{ asset('img/logo.svg') }}" alt="logo" class="w-100">
             </a>
         </div>
@@ -11,8 +11,7 @@
                     <a href="{{ route('admin.dashboard') }}"
                         class="{{ request()->routeIs('admin.dashboard') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2 ">
-                            <img src="{{ request()->routeIs('admin.dashboard') ? asset('img/gridg.svg') : asset('img/grid.svg') }}"
-                                alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.dashboard') ? asset('img/gridg.svg') : asset('img/grid.svg') }}" alt="" class="sicon">
                             <p class="mb-0 ">Dashboard</p>
                         </div>
                     </a>
@@ -21,8 +20,7 @@
                     <a href="{{ route('admin.orders') }}"
                         class="{{ request()->routeIs('admin.orders') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.orders') ? asset('img/ordersg.svg') : asset('img/orders.svg') }}"
-                                alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.orders') ? asset('img/boxg.svg') : asset('img/box.svg') }}" alt="" class="sicon">
                             My Orders
                         </div>
                     </a>
@@ -31,8 +29,7 @@
                     <a href="{{ route('admin.clients') }}"
                         class="{{ request()->routeIs('admin.clients') ? 'active' : '' }} text-decoration-none d-block lnk text-black">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.clients') ? asset('img/clientsg.svg') : asset('img/clients.svg') }}"
-                                alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.clients') ? asset('img/userg.svg') : asset('img/user.svg') }}" alt="" class="sicon">
                             My Clients
                         </div>
                     </a>
@@ -41,8 +38,7 @@
                     <a href="{{ route('admin.payments') }}"
                         class="{{ request()->routeIs('admin.payments') ? 'active' : '' }} text-decoration-none d-block lnk text-black ">
                         <div class="d-flex align-items-center ms-2">
-                            <img src="{{ request()->routeIs('admin.payments') ? asset('img/paymentg.svg') : asset('img/payment.svg') }}"
-                                alt="" class="sicon">
+                            <img src="{{ request()->routeIs('admin.payments') ? asset('img/bankg.svg') : asset('img/bank.svg') }}" alt="" class="sicon">
                             Payments
                         </div>
                     </a>
