67ad17a2590cc1f47729c8d89f6e453b1a8f60e4
shakaib-fabtechsol <shakaib@fabyechsol.com>
2024-05-25 16:29:52 +0500

    investment complete

--- resources/views/User/makeinvestment.blade.php
+++ resources/views/User/makeinvestment.blade.php
@@ -74,7 +74,7 @@
                                     <div class="mt-3 d-flex justify-content-end">
                                         <button type="submit"
                                             class=" px-4 py-2 border-0 rounded-3 green text-white font-semi">
-                                            Validate Order
+                                            Invest
                                         </button>
                                     </div>
                                 </div>
