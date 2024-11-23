510fcd6cbe2dbc35b251a897144baf5cd4efdee8
Shakaib Ishfaq <139215472+shakaib-fabtechsol@users.noreply.github.com>
2024-11-23 12:56:43 +0500

    esg investment top paragraph changed

--- resources/views/ESGInvestments.blade.php
+++ resources/views/ESGInvestments.blade.php
@@ -14,12 +14,12 @@
                 <div class="col-12">
                     <h1 class="font-bold text-black  display-4 text-center" data-aos="fade-right" data-aos-delay="300">
                         ESG Investments</h1>
-                    <p class=" text-center text-center mt-3 fs-5" data-aos="fade-up" data-aos-delay="500">As a liquidity
-                        We help our partners to invest in Africa by delivering tailored services, in line with their
-                        investment strategy and policies. Once you have defined your investment objectives, we will
-                        leverage our local presence and find the right projects for your fund. Investing in Africa can
-                        be challenging, and we are here to help and ease the process for you to focus on your investment
-                        strategy.
+                    <p class=" text-center text-center mt-3 fs-5" data-aos="fade-up" data-aos-delay="500">
+                        We provide tailored solutions for funds seeking to invest in ESG projects across Africa. Our
+                        local presence enables our partners to access projects that align with their specific investment
+                        policies and objectives.
+                        Our Investment process is designed to secure our partners investments against an adverse move on
+                        the FX markets while offering a full transparency on the allocation of the funds.
                     </p>
                 </div>
             </div>
@@ -29,11 +29,11 @@
         <div class="mycontainer py-5">
             <div class="row justify-content-center align-items-center py-5">
                 <div class="col-10 col-sm-8 col-md-6 py-lg-3" data-aos="fade-down" data-aos-delay="500">
-                    <img src="{{asset('img/crane.svg')}}" alt="crane" class="w-100">
+                    <img src="{{asset('img/cranes.svg')}}" alt="crane" class="w-100">
                 </div>
                 <div class="col-md-6 mt-5 mt-md-0 py-lg-3 ps-md-4" data-aos="fade-right" data-aos-delay="300">
                     <h2 class="font-semi fs-1 green-text">The Right Projects</h2>
-                    <p  class="fs-5">
+                    <p class="fs-5">
                         We source projects that align with the Sustainable Development Goals (SDGs) set
                         by the United Nations are selected based on your specific investment objectives, including
                         sector, impact, duration, capital amount, and yield targets. Additionally, we manage the
@@ -44,7 +44,7 @@
             <div class="row justify-content-center flex-column-reverse flex-md-row align-items-center py-5">
                 <div class="col-md-6 py-lg-3 mt-5 mt-md-0 pe-md-4" data-aos="fade-up" data-aos-delay="300">
                     <h2 class="font-semi fs-1 green-text  ">Our Actions</h2>
-                    <p  class="fs-5">
+                    <p class="fs-5">
                         With offices in Western and Central Africa, Ireti Capital directly contributes
                         to the local economy by collaborating closely with local communities.
                         Ireti Capital is a member of Principles for Responsible Investments (PRI), an
@@ -55,7 +55,7 @@
                     </p>
                 </div>
                 <div class="col-md-6 py-lg-3" data-aos="fade-right" data-aos-delay="300">
-                    <img src="{{asset('img/plan.svg')}}" alt="plan" class="w-100">
+                    <img src="{{asset('img/esgplan.svg')}}" alt="plan" class="w-100">
                 </div>
             </div>
             <div class="row align-items-center justify-content-center py-5">
@@ -69,7 +69,7 @@
                     </p>
                 </div>
                 <div class="col-md-8  px-sm-5" data-aos="fade-right" data-aos-delay="300">
-                    <img src="{{ asset('img/main.svg') }}" alt="ngo" class="w-100">
+                    <img src="{{ asset('img/esgmain.svg') }}" alt="ngo" class="w-100">
                 </div>
             </div>
         </div>
